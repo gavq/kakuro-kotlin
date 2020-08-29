e389b279a7ea84db711f33620e9a0346fe4a4b87
g <897522+gavq@users.noreply.github.com>
2020-08-29 10:20:44 +0100

    wip

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,3 +1,44 @@
+fun pad2(n: Int): String {
+    var s = "" + n
+    if (s.length < 2) {
+        return " $s"
+    } else {
+        return s
+    }
+}
+
+interface ICell {
+    fun draw(): String
+}
+
+interface IDown {
+    val down: Int
+}
+
+interface IAcross {
+    val across: Int
+}
+
+class EmptyCell : ICell {
+    override fun toString(): String {
+        return "EmptyCell"
+    }
+
+    override fun draw(): String {
+        return "   -----  "
+    }
+}
+
+class DownCell(override val down: Int) : ICell, IDown {
+    override fun toString(): String {
+        return "DownCell[$down]"
+    }
+
+    override fun draw(): String {
+        return "   " + pad2(down) + "\\--  "
+    }
+}
+
 fun main(args: Array<String>) {
     println("Hello World!")
 }
